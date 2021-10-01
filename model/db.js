9c0a410b5485abeb2eaec7811c875f23027efc47
ElenaPrepodobnaja <73935522+ElenaPrepodobnaja@users.noreply.github.com>
2021-10-01 19:45:49 +0300

    hw-02

--- model/db.js
+++ model/db.js
@@ -5,11 +5,13 @@ class FileContacts {
     constructor (file){
         this.store = path.join(__dirname, file)
     }
+
     async read() {
         const result = await fs.readFile(this.store, 'utf8')
     const data = JSON.parse(result)
     return data 
     }
+
     async write (data) {
         await fs.writeFile(this.store, JSON.stringify(data))
     }
