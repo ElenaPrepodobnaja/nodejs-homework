d2d0b41b1db4a2aec4ac9ce689f5b550fef6f936
ElenaPrepodobnaja <73935522+ElenaPrepodobnaja@users.noreply.github.com>
2021-10-08 20:58:04 +0300

    создание подключения к MongoDB при помощи Mongoose

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,95 +1,29 @@
 const express = require('express')
 const router = express.Router()
-const Contacts = require('../../model/index')
+const {
+  getContacts,
+  getContact,
+  saveContact,
+  removeContact,
+  updateContact,
+  updateStatusContact,
+} = require('../../controllers/contacts')
 const { validateContact, validateStatusContact, validateId } = require('./validation')
 
-router.get('/', async (req, res, next) => {
-  try {
-    console.log(req.method)
-    const contacts = await Contacts.listContacts()
-    res.json({ status: 'success', code: 200, data: { contacts } })
-  } catch (error) {
-    next(error)
-  }
-})
+router.get('/', getContacts)
 
-router.get('/:id', validateId, async (req, res, next) => {
-  try {
-    const contact = await Contacts.getContactById(req.params.id)
-    if (contact) {
-      return res
-        .status(200)
-        .json({ status: 'success', code: 200, data: { contact } })
-    }
-    return res
-      .status(404)
-      .json({ status: 'error', code: 404, message: 'Not Found' })
-  } catch (error) {
-    next(error)
-  }
-})
+router.get('/:id', validateId, getContact)
 
-router.post('/', validateContact, async (req, res, next) => {
-  try {
-    const contact = await Contacts.addContact(req.body)
-    res.status(201).json({ status: 'success', code: 201, data: { contact } })
-  } catch (error) {
-    next(error)
-  }
-})
+router.post('/', validateContact, saveContact)
 
-router.delete('/:id', validateId, async (req, res, next) => {
-  try {
-    const contact = await Contacts.removeContact(req.params.id)
-    if (contact) {
-      return res
-        .status(200)
-        .json({ status: 'success', code: 200, data: { contact } })
-    }
-    return res
-      .status(404)
-      .json({ status: 'error', code: 404, message: 'Not Found' })
-  } catch (error) {
-    next(error)
-  }
-})
+router.delete('/:id', validateId, removeContact)
 
-router.put('/:id', validateId, validateContact, async (req, res, next) => {
-  try {
-    console.log(req.method)
-    const contact = await Contacts.updateContact(req.params.id, req.body)
-    if (contact) {
-      return res
-        .status(200)
-        .json({ status: 'success', code: 200, data: { contact } })
-    }
-    return res
-      .status(404)
-      .json({ status: 'error', code: 404, message: 'Not Found' })
-  } catch (error) {
-    next(error)
-  }
-})
+router.put('/:id', [validateId, validateContact], updateContact)
 
 router.patch(
   '/:id/favorite/',
-  validateId,
-  validateStatusContact,
-  async (req, res, next) => {
-    try {
-      const contact = await Contacts.updateContact(req.params.id, req.body)
-      if (contact) {
-        return res
-          .status(200)
-          .json({ status: 'success', code: 200, data: { contact } })
-      }
-      return res
-        .status(404)
-        .json({ status: 'error', code: 404, message: 'Not Found' })
-    } catch (error) {
-      next(error)
-    }
-  },
+  [validateId, validateStatusContact],
+  updateStatusContact,
 )
 
 module.exports = router
